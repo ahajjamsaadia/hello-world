342442fdb4cba62c5367924026c1d5150ed3eddf
ahajjamsaadia <sa.ahajjam@gmail.com>
2022-03-24 14:33:30 +0100

    Addition 2 nbrs

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {    
+
+    int number1=1, number2=2, sum;
+ 
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
